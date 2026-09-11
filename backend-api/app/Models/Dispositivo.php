126d0a10111a50d2a65ca941d0d6b295c0bcbd31
kevenklain <kevennmc@gmail.com>
2026-09-11 14:46:38 -0300

    Ajusta timeout de comunicação dos dispositivos

--- backend-api/app/Models/Dispositivo.php
+++ backend-api/app/Models/Dispositivo.php
@@ -69,11 +69,11 @@ class Dispositivo extends Model
 
         $segundos = Carbon::parse($this->ultima_comunicacao)->diffInSeconds(now());
 
-        if ($segundos <= 30) {
+        if ($segundos <= 10) {
             return 'online';
         }
 
-        if ($segundos <= 60) {
+        if ($segundos <= 30) {
             return 'sem_comunicacao';
         }
 
